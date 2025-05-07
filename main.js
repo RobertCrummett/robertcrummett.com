813a871d5801bba35094c12f116e4d1cc95178bc
Robert Crummett <rncrummett5@gmail.com>
2025-05-07 08:57:36 -0600

    Experimenting with textures and materials

--- main.js
+++ main.js
@@ -3,23 +3,42 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
-    35, window.innerWidth / window.innerHeight, 0.1, 30
+    35, window.innerWidth / window.innerHeight, 0.1, 1000
 )
 camera.position.z = 10
 
-// RGB Axis indicator
-const axesHelper = new THREE.AxesHelper();
+// Create custom geometry
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const torusKnotGeometry = new THREE.TorusKnotGeometry(0.5, 0.15, 100, 16)
+const planeGeometry = new THREE.PlaneGeometry(1, 1)
+
+const material = new THREE.MeshPhysicalMaterial()
+material.color = new THREE.Color('green')
+material.metalness = 0.1
+material.roughness = 0
+material.reflectivity = 0
+material.clearcoat = 0.4
+
+const mesh = new THREE.Mesh(geometry, material)
+const mesh2 = new THREE.Mesh(torusKnotGeometry, material)
+mesh2.position.x = 1.5
+const mesh3 = new THREE.Mesh(planeGeometry, material)
+mesh3.position.x = -1.5
+
+scene.add(mesh)
+scene.add(mesh2)
+scene.add(mesh3)
+
+const light = new THREE.AmbientLight(0xffffff, 1)
+scene.add(light)
+
+const pointLight = new THREE.PointLight(0xffffff, 10)
+pointLight.position.set(1, 1, 1)
+scene.add(pointLight)
+
+const axesHelper = new THREE.AxesHelper()
 axesHelper.setColors(0xff0000, 0x00ff00, 0x0000ff)
 scene.add(axesHelper)
-
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-const cubeMaterial = new THREE.MeshBasicMaterial({
-    color: 0xffaaff,
-    wireframe: true
-})
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
-
-scene.add(cubeMesh)
 
 const canvas = document.querySelector('canvas.threejs')
 const renderer = new THREE.WebGLRenderer({
@@ -40,7 +59,17 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix()
 })
 
+const clock = new THREE.Clock()
+let previousTime = 0
+
 const renderLoop = () => {
+    const currentTime = clock.getElapsedTime()
+    const delta = currentTime - previousTime
+    previousTime = currentTime
+
+    // cubeMesh.rotation.y += THREE.MathUtils.degToRad(1) * delta * 20
+    // cubeMesh.scale.x = Math.sin(currentTime)
+
     controls.update()
     renderer.render(scene, camera)
     window.requestAnimationFrame(renderLoop)
