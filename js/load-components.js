55df0edd8d39c1823f1a8e8646cb82c6b93d3b6f
Robert Crummett <rncrummett5@gmail.com>
2025-05-16 14:07:36 -0600

    Colorscheme selected - vibe coding to victory

---
+++ js/load-components.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    Promise.all([
+        fetch('components/nav.html').then(res => res.text()),
+        fetch('components/footer.html').then(res => res.text())
+    ]).then(([navHTML, footerHTML]) => {
+        document.body.insertAdjacentHTML('afterbegin', navHTML);
+        document.body.insertAdjacentHTML('beforeend', footerHTML);
+        
+        highlightActiveLinks('.navbar a');
+        highlightActiveLinks('.footer-nav a');
+    });
+
+    function highlightActiveLinks(selector) {
+        const links = document.querySelectorAll(selector);
+        const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+        
+        links.forEach(link => {
+            if (link.getAttribute('href') === currentPage) {
+                link.classList.add('active');
+            }
+        });
+    }
+});
